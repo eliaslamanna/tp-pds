7b1a4ff0fb8665673aeeaff80a2727acfdff0b01
eliaslamanna <eliaslamanna@hotmail.com>
2024-04-18 22:08:33 -0300

    springboot

---
+++ src/test/java/com/example/tppds/TpPdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tppds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpPdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
