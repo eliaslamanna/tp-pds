997d3624b701be06e7ef9673e478860987ff309f
eliaslamanna <eliaslamanna@hotmail.com>
2024-04-21 19:42:50 -0300

    inscripcionController

---
+++ src/main/java/com/example/tppds/InscripcionController.java
@@ -0,0 +1,21 @@
+package com.example.tppds;
+
+public class InscripcionController {
+
+    public void asignarEstudianteACurso(int legajo, String nombreMat) {
+
+    }
+
+    private Estudiante buscarEstudiante(int legajo) {
+
+    }
+
+    private Materia buscarMateria(String nombreMat) {
+
+    }
+
+    private Curso buscarCurso(Materia materia) {
+        
+    }
+
+}
